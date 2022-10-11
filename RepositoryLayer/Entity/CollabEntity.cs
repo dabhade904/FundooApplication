ca635e91b73769befa902d69487def2313550c73
dabhade904 <dabhade904@gmail.com>
2022-10-11 11:41:18 +0530

    Added : created database using migration

---
+++ RepositoryLayer/Entity/CollabEntity.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace RepositoryLayer.Entity
+{
+    public class CollabEntity
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public long collabId { get; set; }
+        [Display(Name = "Email address")]
+        [Required(ErrorMessage = "The email address is required")]
+        [EmailAddress(ErrorMessage = "Invalid Email Address")]
+        public string collabratorEmail { get; set; }
+        [ForeignKey("NoteEntity")]
+        public long noteID { get; set; }
+        public virtual NoteEntity NoteEntity { get; set; }
+    }
+}
